631e73e5486a440ccf36bd5ea25d14a1113d7e97
mikeisfake <mikescooper@icloud.com>
2020-03-02 02:32:52 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,27 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function range(n, min, max) {
+  return n >= min && n <= max
+}
+
+function greet(string) {
+  let time = parseInt(string.split(':'))
+  if (range(time, 0, 11)) {
+    return "Good Morning"
+  }
+  if (range(time, 12, 17)) {
+    return "Good Afternoon"
+  }
+  if (range(time, 18, 24)) {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  let node = document.getElementById('greeting')
+  node.innerText = string
+}
