5fabd475d281a8b840d93ea89184efc88334866c
Dylan Crawshaw <dcrawsh@gmail.com>
2020-03-05 22:22:11 -0500

    Done.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function greet(timeStr) {
    *
    */
   const hour = parseInt(timeStr, 10);
-  if ( hour < 12) return "Good Morning"
-  if ( hour > 17) return "Good Evening"
-  return "Good Afternoon"
+  if ( hour < 12) return "Good Morning";
+  if ( hour > 17) return "Good Evening";
+  return "Good Afternoon";
 }
