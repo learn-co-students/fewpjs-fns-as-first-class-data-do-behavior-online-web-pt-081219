0ab49e704468e1fef01460743ee25c86ea39a582
Phyllis Kan <Phyllis Kan>
2020-03-08 20:48:33 -0500

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
 
 function greet(time) {
 	let hour = time.split(':')[0];
@@ -23,6 +22,7 @@ function greet(time) {
 	}
 }
 
+/* Write your implementation of displayMessage() */
 function displayMessage(message) {
 	document.querySelector('#greeting').innerText = message;
 }
