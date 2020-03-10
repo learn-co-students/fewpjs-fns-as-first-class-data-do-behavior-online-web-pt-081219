7ad2b7b9c4ae12335be4ce0bb6fe2c0110a1ab45
Mars Le <hp.mars.le@gmail.com>
2020-03-10 20:11:42 +0000

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  let t = time.split(":")
+  let h = parseInt(t[0])
+  let m = parseInt(t[1])
+  
+  if (h < 12){
+    return "Good Morning"
+  } else if (h >= 12 && h < 17){
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+  
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(greetingText){
+  let gNode = document.getElementById("greeting")
+  gNode.innerText = greetingText
+}
